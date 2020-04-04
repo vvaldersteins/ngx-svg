59f63538441cb6f392a14b3680c3c0536b2a8623
Valters Valdersteins <valters@toptal.com>
2020-04-04 10:07:59 +0300

    Updated karma test configuration

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
+// karma.conf.js
+const path = require('path');
+ 
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -8,7 +8,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-coverage'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-jasmine-html-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -16,13 +15,10 @@ module.exports = function (config) {
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    reporters: ['progress', 'coverage'],
-    preprocessors: {
-      'src/app/**/*.ts': ['coverage']
-    },
-    coverageReporter: {
-      type: 'lcov',
-      dir: 'coverage/'
+    reporters: ['dots', 'coverage-istanbul'],
+    coverageIstanbulReporter: {
+      reports: ['lcovonly'],
+      dir: path.join(__dirname, 'coverage'),
     },
     customLaunchers: {
       ChromeHeadlessNoSandbox: {

fbbd26dc045e5c4aa05989ce72fdfc28faa27e8c
Valters Valdersteins <valters@toptal.com>
2021-01-21 11:33:04 +0200

    Updated demo

--- src/demo/src/app/app.module.ts
+++ src/demo/src/app/app.module.ts
@@ -68,9 +68,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(
-      routes
-    ),
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     NgxSvgModule,
     FormsModule
   ],
